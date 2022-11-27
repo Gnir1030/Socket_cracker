5137ff19ceb7afba3430c5fcc9f8790a7913e558
Louis Lim <llim3@ucsc.edu>
2022-11-26 21:51:31 -0800

    initial commit

---
+++ src/cracker.cc
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2018-2022 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without 
+ * the express written permission of the copyright holder.
+ */
+
+#include <iostream>
+
+#include "cracker.h"
+
+/**
+ * @brief main function of yoiur cracker
+ * 
+ * No command line arguments are passed
+ * 
+ * @return int not checked by test harness
+ */
+int main() {
+    std::cout << "Not implemented!" << std::endl;
+}
