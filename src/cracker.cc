cff63eb0855b153389512c8629b9310c0939064c
louis01056 <louis01056@gmail.com>
2022-11-27 23:58:46 -0800

    recv

--- src/cracker.cc
+++ src/cracker.cc
@@ -47,7 +47,7 @@ int main() {
     std::cout << buffer.port << std::endl;
 
     char password[4];
-
+    char password2[2];
     //char a[MAX_HASHES][HASH_LENGTH+1] = buffer.passwds;
 
     //std::cout << buffer.passwds.size() << std::endl;
@@ -55,7 +55,9 @@ int main() {
     for(unsigned int i = 0; i < ntohl(buffer.num_passwds); i++){
         std::cout << buffer.passwds[i] <<std::endl;
         crack(buffer.alphabet, buffer.passwds[i], password);
+        crack(buffer.alphabet, buffer.passwds[i], password2);
         std::cout << password << std::endl;
+         std::cout << password2 << std::endl;
     }
 
     close(sockfd);
