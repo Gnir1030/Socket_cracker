fd55017b9f32d01893b4f70c2829716f8ebac94c
louis01056 <louis01056@gmail.com>
2022-11-28 21:50:50 -0800

    crypt

--- src/cracker.cc
+++ src/cracker.cc
@@ -84,7 +84,8 @@ int main() {
     }
 */
 
-    char* salt = newBuffer.passwds[0].substr(0,2);
+    char* salt;
+    memcpy( salt, &newBuffer.passwds[0][0], 2 );
     char* hash = crypt(newBuffer.passwds[0], salt);
     std::cout << "HASH: " << hash << std::endl;
 
