6ca9003da4c0e261f9b741a05949b4ae45b7969f
louis01056 <louis01056@gmail.com>
2022-11-29 14:53:38 -0800

    pcrack

--- src/cracker.cc
+++ src/cracker.cc
@@ -4,7 +4,7 @@
  * You may not use, distribute, publish, or modify this code without 
  * the express written permission of the copyright holder.
  */
-#define _GNU_SOURCE
+//#define _GNU_SOURCE
 #include <crypt.h>
 #include <iostream>
 #include <string.h>
@@ -30,7 +30,8 @@ void pcrack(const char *alphabet, const char *hash, char *passwd, unsigned int s
     char a[5]; //4 char password
     char salt[3];
     memcpy( salt, &hash[0], 2 ); // first two character as salt
-    struct crypt_data data = NULL;
+    struct crypt_data data;
+    data.initialized = 0;
     a[4] = '\0';
 
     for(unsigned int i = threads; i < MAX_HASHES; i = i + split){
@@ -42,7 +43,7 @@ void pcrack(const char *alphabet, const char *hash, char *passwd, unsigned int s
                 for(unsigned int p = 0; p < ALPHABET_LEN; p++){
                     a[3] = alphabet[p];
                     char hc[14];
-                    strcpy(hc, crypt(a, salt));
+                    strcpy(hc, crypt_r(a, salt, data));
                     int cmp = strcmp(hc, hash);
                     if(a[0] == 'z' && a[1] == 'U' && a[2] == 'S' && a[3] == '0'){
                         std::cout << "\nthread: " << threads << "\ncharacter: " << a << "\nstrcmp(crypt(a, salt), hash): "<< cmp << "\ncrypt(a,salt):" << crypt(a, salt) << "," << hc
