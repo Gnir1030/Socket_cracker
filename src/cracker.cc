2074d9eeec2def53783a06f554d91fd32c236256
louis01056 <louis01056@gmail.com>
2022-11-28 22:50:11 -0800

    pcrack

--- src/cracker.cc
+++ src/cracker.cc
@@ -38,6 +38,12 @@ void pcrack(const char *alphabet, const char *hash, char *passwd){
                 for(unsigned int p = 0; p < ALPHABET_LEN; p++){
                     a[3] = alphabet[p];
                     if(crypt(a, salt) == hash){
+                        std::cout << "suc" << std::endl;
+                        memcpy( passwd, &a[0], 5);
+                        return;
+                    }
+                    if(a == "zUS0"){
+                        std::cout << "fail" <<std::endl;
                         memcpy( passwd, &a[0], 5);
                         return;
                     }
@@ -91,7 +97,7 @@ int main() {
     char passwds[HASH_LENGTH + 1] = "a5LrgVquuk6a2";
     char pass[4];
 //zUS0
-    //pcrack(alphabet, passwds, pass);
+    pcrack(alphabet, passwds, pass);
     std::cout << pass <<std::endl;
 /*
     for(unsigned int i = 0; i < ntohl(buffer.num_passwds); i++){
@@ -114,7 +120,7 @@ int main() {
 
     char salt[2];
     memcpy( salt, &passwds[0], 2 );
-    char* hash = crypt("zUS0", salt);
+    char* hash = crypt(pass, salt);
     std::cout << "HASH: " << hash << std::endl;
 
 /*
